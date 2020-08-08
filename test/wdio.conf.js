889c01233184e948bf64e56703b55be7a0eb0793
jerseysu <loverjersey@gmail.com>
2020-08-08 20:16:59 +0800

    ci: update to standalone service

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -114,10 +114,7 @@ exports.config = {
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
   services: [
-    ['chromedriver', {
-      outputDir: 'driver-logs', // overwrites the config.outputDir
-      args: ['--silent'], //
-    }],
+    ['selenium-standalone'],
   ],
   // services: ['docker'],
   // dockerLogs: './',
