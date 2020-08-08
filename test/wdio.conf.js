83390f23172fc7f4df6d939ecaf1358970c7de8a
jerseysu <loverjersey@gmail.com>
2020-08-08 18:42:34 +0800

    ci: update chromedriver

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -44,7 +44,7 @@ exports.config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 10,
+  maxInstances: 1,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -54,9 +54,12 @@ exports.config = {
     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
     // grid with only 5 firefox instances available you can make sure that not more than
     // 5 instances get started at a time.
-    maxInstances: 5,
+    'maxInstances': 1,
     //
-    browserName: 'chrome',
+    'browserName': 'chrome',
+    'goog:chromeOptions': {
+      args: ['headless', 'disable-gpu'],
+    },
     // If outputDir is provided WebdriverIO can capture driver session logs
     // it is possible to configure which logTypes to include/exclude.
     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
@@ -110,16 +113,22 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ['docker'],
-  dockerLogs: './',
-  dockerOptions: {
-    image: 'selenium/standalone-chrome',
-    healthCheck: 'http://localhost:4444',
-    options: {
-      p: ['4444:4444'],
-      shmSize: '2g',
-    },
-  },
+  services: [
+    ['chromedriver', {
+      outputDir: 'driver-logs', // overwrites the config.outputDir
+      args: ['--silent'], //
+    }],
+  ],
+  // services: ['docker'],
+  // dockerLogs: './',
+  // dockerOptions: {
+  //   image: 'selenium/standalone-chrome',
+  //   healthCheck: 'http://localhost:4444',
+  //   options: {
+  //     p: ['4444:4444'],
+  //     shmSize: '2g',
+  //   },
+  // },
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
