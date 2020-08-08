cd4711cc775d65a01d043206a9787f90259cf594
jerseysu <loverjersey@gmail.com>
2020-08-08 22:01:37 +0800

    fix(travis): npm ci error

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -53,12 +53,17 @@ exports.config = {
   capabilities: [{
     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
     // grid with only 5 firefox instances available you can make sure that not more than
-    // 5 instances get started at a time.
+    // 5 instances get started at a time.g
     'maxInstances': 1,
     //
     'browserName': 'chrome',
     'goog:chromeOptions': {
-      args: process.env.CI ? ['--headless', '--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--window-size=1920,1080'] : [],
+      args: process.env.CI ? [
+        '--headless',
+        '--no-sandbox',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--window-size=1920,1080'] : [],
     },
     // If outputDir is provided WebdriverIO can capture driver session logs
     // it is possible to configure which logTypes to include/exclude.
