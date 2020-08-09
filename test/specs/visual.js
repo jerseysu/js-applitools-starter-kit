0883e06fdd951e72ad8c42794efe750b21099a7f
jerseysu <loverjersey@gmail.com>
2020-08-09 12:16:12 +0800

    feat: add new test

--- test/specs/visual.js
+++ test/specs/visual.js
@@ -15,11 +15,23 @@ const {Configuration} = require('@applitools/eyes-selenium');
 
 const buildNumber = process.env.TRAVIS_JOB_NUMBER || 'local';
 
-let browser; let eyes;
+let browser; let eyes; let configuration;
 
 describe('wdio6', function() {
   before(async () => {
     await chromedriver.start();
+    configuration = await new Configuration()
+        // .addDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.PORTRAIT)
+        .setApiKey(process.env.APPLITOOLS_API_KEY)
+        .setAppName(`Demo App`)
+        .setViewportSize({width: 1024, height: 768});
+
+    const runner = await new ClassicRunner();
+
+    eyes = await new Eyes(runner);
+    await eyes.setConfiguration(configuration);
+    await eyes.setLogHandler(new ConsoleLogHandler(true));
+    await eyes.setLogHandler(new FileLogHandler(true, 'test/logs/eyes.log', false));
   });
 
   beforeEach(async () => {
@@ -29,24 +41,12 @@ describe('wdio6', function() {
       },
     };
 
-    const configuration = await new Configuration()
-        // .addDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.PORTRAIT)
-        .setApiKey(process.env.APPLITOOLS_API_KEY)
-        .setAppName('Demo App')
-        .setTestName(`Smoke Test #${buildNumber}`)
-        .setViewportSize({width: 1024, height: 768});
-
-    const runner = await new ClassicRunner();
-    eyes = await new Eyes(runner);
-    await eyes.setConfiguration(configuration);
-    await eyes.setLogHandler(new ConsoleLogHandler(true));
-    await eyes.setLogHandler(new FileLogHandler(true, 'test/logs/eyes.log', false));
-
     browser = await remote(chrome);
   });
 
   afterEach(async () => {
     await browser.deleteSession();
+    await eyes.close(false);
     await eyes.abortIfNotClosed();
 
     const results = await eyes.getRunner().getAllTestResults(false);
@@ -58,7 +58,9 @@ describe('wdio6', function() {
     await chromedriver.stop();
   });
 
-  it('Test - Basic Check', async () => {
+  it(`Test - Basic Check - #${buildNumber}`, async () => {
+    await configuration.setTestName(`Basic`);
+    await eyes.setConfiguration(configuration);
     await eyes.open(browser);
 
     await browser.url('https://demo.applitools.com');
@@ -77,16 +79,33 @@ describe('wdio6', function() {
     // Visual checkpoint #2.
     await eyes.check('App Window', Target.window().fully());
 
-    await eyes.closeAsync();
+    // await eyes.closeAsync();
   });
 
-  it('Test - Baidu', async () => {
+  it(`Test - Baidu - #${buildNumber}`, async () => {
+    await configuration.setTestName(`Baidu`);
+    await eyes.setConfiguration(configuration);
     await eyes.open(browser);
     await browser.url('https://www.baidu.com');
 
-    // Visual checkpoint #1.
+    // Visual checkpoint #3.
     await eyes.check('Baidu', Target.window().fully());
 
-    await eyes.closeAsync();
+    const searchButton = await browser.$('a#hotsearch-refresh-btn');
+    await searchButton.click();
+
+    // Visual checkpoint #4.
+    await eyes.check('Baidu2', Target.window().fully());
+    // await eyes.closeAsync();
+  });
+
+  it(`Test - Y!TW - #${buildNumber}`, async () => {
+    await configuration.setTestName(`Y!TW`);
+    await eyes.setConfiguration(configuration);
+    await eyes.open(browser);
+    await browser.url('https://tw.yahoo.com');
+
+    // Visual checkpoint #5.
+    await eyes.check('Y! TW', Target.window());
   });
 });
